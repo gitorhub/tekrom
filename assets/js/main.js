56f2cec5c7988da3d09f6a1eebe635489da21c55
Hasan ÜNAL <hasanunalweb@gmail.com>
2021-07-30 16:13:41 +0300

    maps end

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,10 @@
     };
     SliceSlider.init();
 })(jQuery);
+
+/* tooltips everywhere */
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
